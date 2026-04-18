255ca5f4d943f8ad57ee67754b3305a6df9459c2
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-18 14:25:11 +0800

    短信登陆-实现短信验证码登陆和注册功能

--- src/main/java/com/hmdp/service/IUserService.java
+++ src/main/java/com/hmdp/service/IUserService.java
@@ -1,6 +1,7 @@
 package com.hmdp.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
+import com.hmdp.dto.LoginFormDTO;
 import com.hmdp.dto.Result;
 import com.hmdp.entity.User;
 
@@ -16,9 +17,7 @@ import javax.servlet.http.HttpSession;
  */
 public interface IUserService extends IService<User> {
 
-    /**
-     * 发送手机验证码
-     */
     Result sendCode(String phone, HttpSession session);
 
+    Result login(LoginFormDTO loginForm, HttpSession session);
 }
