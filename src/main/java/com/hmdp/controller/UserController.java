0d11e77f52c2c037aa18e986f01346decd163ecf
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-18 13:37:14 +0800

    短信登陆-实现发送短信验证码功能

--- src/main/java/com/hmdp/controller/UserController.java
+++ src/main/java/com/hmdp/controller/UserController.java
@@ -36,8 +36,8 @@ public class UserController {
      */
     @PostMapping("code")
     public Result sendCode(@RequestParam("phone") String phone, HttpSession session) {
-        // TODO 发送短信验证码并保存验证码
-        return Result.fail("功能未完成");
+        //发送短信验证码并保存验证码
+        return userService.sendCode(phone, session);
     }
 
     /**
@@ -46,7 +46,7 @@ public class UserController {
      */
     @PostMapping("/login")
     public Result login(@RequestBody LoginFormDTO loginForm, HttpSession session){
-        // TODO 实现登录功能
+        // 实现登录功能
         return Result.fail("功能未完成");
     }
 
