3f048d1f4f7d2c38124583a3bdf978aedaa75342
desire_zzz <145351970+zxA6@users.noreply.github.com>
2026-04-22 11:08:46 +0800

    分布式锁-使用Lua脚本实现判断标识与释放锁命令的原子性

--- src/main/java/com/hmdp/utils/SimpleRedisLock.java
+++ src/main/java/com/hmdp/utils/SimpleRedisLock.java
@@ -1,8 +1,11 @@
 package com.hmdp.utils;
 
 import cn.hutool.core.lang.UUID;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 
+import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
 public class SimpleRedisLock implements ILock {
@@ -17,6 +20,12 @@ public class SimpleRedisLock implements ILock {
 
     private static final String KEY_PREFIX = "lock:";
     private static final String ID_PREFIX = UUID.randomUUID().toString(true) + "-";
+    private static final DefaultRedisScript<Long> UNLOCK_SCRIPT;
+    static {
+        UNLOCK_SCRIPT = new DefaultRedisScript<>();
+        UNLOCK_SCRIPT.setLocation(new ClassPathResource("unlock.lua"));
+        UNLOCK_SCRIPT.setResultType(Long.class);
+    }
 
     @Override
     public boolean tryLock(long timeoutSec) {
@@ -28,6 +37,17 @@ public class SimpleRedisLock implements ILock {
         return Boolean.TRUE.equals(success);
     }
 
+    //使用Lua脚本实现判断标识与释放锁的一致性
+    @Override
+    public void unlock() {
+        //调用Lua脚本
+        stringRedisTemplate.execute(
+                UNLOCK_SCRIPT,
+                Collections.singletonList(KEY_PREFIX + name),
+                ID_PREFIX + Thread.currentThread().getId());
+    }
+
+    /*
     @Override
     public void unlock() {
         //获取线程标识
@@ -39,5 +59,5 @@ public class SimpleRedisLock implements ILock {
             //一致，释放锁
             stringRedisTemplate.delete(KEY_PREFIX + name);
         }
-    }
+    }*/
 }
